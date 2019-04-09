a0651dbf906bac5e296f8a0059d14e6630b2645b
Soma Mbadiwe <somasystemsng@gmail.com>
2019-04-09 13:32:09 -0400

    Siamese training well now

--- keras_squeezenet/squeezenet_lite.py
+++ keras_squeezenet/squeezenet_lite.py
@@ -118,46 +118,12 @@ def SqueezeNet(include_top=True,
 def euclidean_distance(inputs):
     assert len(inputs) == 2, 'Euclidean distance needs 2 inputs, {} given'.format(len(inputs))
     u, v = inputs
-    return K.sqrt(K.sum((K.square(u - v)), axis=1, keepdims=True))
+    return K.sqrt(K.sum((K.square(u - v)), axis=1, keepdims=True))  # (?, 1)
 
 
 def contrastive_loss(y_true, y_pred):
     margin = 1.
     return K.mean((1. - y_true) * K.square(y_pred) + y_true * K.square(K.maximum(margin - y_pred, 0.)))
-
-
-def dd_make_img_pair(fpaths, identical=True):
-    folder1 = folder2 = np.random.choice(fpaths)
-    if not identical:
-        while os.path.samefile(folder1, folder2):
-            folder2 = np.random.choice(fpaths)
-    rgb_path1 = rgb_path2 = np.random.choice(glob(pjoin(folder1, "*.bmp")))
-    while os.path.samefile(rgb_path1, rgb_path2):
-        rgb_path2 = np.random.choice(glob(pjoin(folder2, "*.bmp")))
-    depth_path1 = rgb_path1[:-5] + "d.dat"
-    depth_path2 = rgb_path2[:-5] + "d.dat"
-
-    # process image 1
-    rgb1 = Image.open(rgb_path1)
-    rgb1.thumbnail((640, 480))
-    rgb1 = np.asarray(rgb1)[140:340, 220:420, :3]
-    depth1 = pd.read_csv(depth_path1, sep='\t', header=None)
-    depth1[(depth1 > 3000) | (depth1 == -1)] = 3000
-    depth1 = depth1.values[140:340, 220:420]
-    depth1 = (depth1 - np.mean(depth1)) / np.max(depth1)
-    rgbd1 = np.dstack((rgb1, depth1))
-
-    # process image 2
-    rgb2 = Image.open(rgb_path1)
-    rgb2.thumbnail((640, 480))
-    rgb2 = np.asarray(rgb2)[140:340, 220:420, :3]
-    depth2 = pd.read_csv(depth_path2, sep='\t', header=None)
-    depth2[(depth2 > 3000) | (depth2 == -1)] = 3000
-    depth2 = depth2.values[140:340, 220:420]
-    depth2 = (depth2 - np.mean(depth2)) / np.max(depth2)
-    rgbd2 = np.dstack((rgb2, depth2))
-
-    return np.array([rgbd1, rgbd2])
 
 
 def run():
@@ -212,36 +178,57 @@ def run():
             pickle.dump(results.history, f)
         save_model(model, squeezenet_model_file)
 
-    with tf.device("/cpu:0"):
-        model.summary()
-
     # Build the siamese architecture
     model.layers.pop()
+    model_cut = Model(name="sqzn_no_softmax", inputs=model.input, outputs=model.layers[-1].output)
+    model_cut.load_weights(squeezenet_model_file, by_name=True)
+    # with tf.device("/cpu:0"):
+    #     model_cut.summary()
+
     input_shape = x_train.shape[1:]
+
     im_in1 = Input(shape=input_shape)
     im_in2 = Input(shape=input_shape)
-    feat_x1 = model(im_in1)
-    feat_x2 = model(im_in2)
+    feat_x1 = model_cut(im_in1)
+    feat_x2 = model_cut(im_in2)
+    lambda_merge = Lambda(euclidean_distance, output_shape=(1,))([feat_x1, feat_x2])
 
-    lambda_merge = Lambda(euclidean_distance, output_shape=input_shape)([feat_x1, feat_x2])
+    siamese = Model(name="siamese", inputs=[im_in1, im_in2], outputs=lambda_merge)
+    with tf.device("/cpu:0"):
+        siamese.summary()
 
-    siamese = Model(input=[im_in1, im_in2], output=lambda_merge, name="siamese")
-    siamese.summary()
-
-    siamese.compile(optimizer=optimizer, loss=contrastive_loss)
+    siamese.compile(optimizer=optimizer, loss=contrastive_loss, metrics=['accuracy'])
 
     def make_img_pair(identical, from_train):
-        # TODO: Select the image pairs
+        """Select the image pairs"""
+        label = np.random.randint(0, num_classes)
         if identical:
             if from_train:
-                pass
+                idx = np.nonzero(y_train[:, label] == 1)[0]
             else:
-                pass
+                idx = np.nonzero(y_test[:, label] == 1)[0]
+
+            # pick any two indexes randomly
+            id1 = np.random.randint(0, idx.shape[0])
+            id2 = np.random.randint(0, idx.shape[0])
+            while id1 == id2:
+                id2 = np.random.randint(0, idx.shape[0])
         else:
             if from_train:
-                pass
+                idx1 = np.nonzero(y_train[:, label] == 1)[0]
+                idx2 = np.nonzero(y_train[:, (label + 1) % num_classes] == 1)[0]
             else:
-                pass
+                idx1 = np.nonzero(y_test[:, label] == 1)[0]
+                idx2 = np.nonzero(y_train[:, (label + 1) % num_classes] == 1)[0]
+
+            # pick any two indexes randomly
+            id1 = np.random.randint(0, idx1.shape[0])
+            id2 = np.random.randint(0, idx2.shape[0])
+
+        if from_train:
+            return np.array([x_train[id1], x_train[id2]])
+        else:
+            return np.array([x_test[id1], x_test[id2]])
 
     def generator(from_train):
         while True:
@@ -250,19 +237,20 @@ def run():
             identical = True
             for _ in range(batch_size):
                 X.append(make_img_pair(identical, from_train))
-                y.append(np.array([0.]))
+                y.append(np.array([0. if identical else 1.]))
                 identical = not identical
             X = np.asarray(X)
             y = np.asarray(y)
             yield [X[:, 0], X[:, 1]], y
 
+    epochs = 100
     callbacks = [
         LearningRateScheduler(partial(getitem, tuple(take(epochs, concat(
             repeat(0.01, 1), repeat(0.1, 99), repeat(0.01, 50), repeat(0.001)))))),
         ModelCheckpoint(filepath=squeezenet_model_file),
         TensorBoard(log_dir="./siam_log", batch_size=batch_size)
     ]
-    outputs = siamese.fit_generator(generator(from_train=True),
+    outputs = siamese.fit_generator(generator(from_train=True), initial_epoch=0,
                                     steps_per_epoch=x_train.shape[0] // batch_size,
                                     epochs=epochs,
                                     validation_data=generator(from_train=False),
