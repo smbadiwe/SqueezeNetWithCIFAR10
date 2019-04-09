486eb7c0e9349a8477dfaa44c132b7bf007cf762
Soma Mbadiwe <somasystemsng@gmail.com>
2019-04-08 20:57:36 -0400

    Implemented a Siamese network based on SqueezeNet

--- keras_squeezenet/squeezenet_lite.py
+++ keras_squeezenet/squeezenet_lite.py
@@ -1,11 +1,27 @@
 from keras_applications.imagenet_utils import _obtain_input_shape
-from keras import backend as K
-from keras.layers import Input, Convolution2D, MaxPooling2D, Activation, GlobalAveragePooling2D, Dense, Dropout
-from keras.layers import concatenate, warnings
-from keras.models import Model
+from keras.layers import concatenate,Input, BatchNormalization, Convolution2D, MaxPooling2D, Activation, GlobalAveragePooling2D, Dense, Lambda
+from keras.datasets import cifar10
+from keras.utils import to_categorical
 from keras.engine.topology import get_source_inputs
-from keras.utils import get_file
+import os
+import glob
 
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from mlxtend.plotting import plot_confusion_matrix
+import numpy as np
+import pickle
+from funcy import concat, partial, repeat, take
+from keras.callbacks import LearningRateScheduler, TensorBoard, ModelCheckpoint
+from keras.models import Model, save_model, load_model
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from operator import getitem
+import tensorflow as tf
+from keras import backend as K
+
+
+pjoin = os.path.join
 sq1x1 = "squeeze1x1"
 exp1x1 = "expand1x1"
 exp3x3 = "expand3x3"
@@ -26,12 +42,15 @@ def fire_module(x, fire_id, squeeze=16, expand=64):
         channel_axis = 3
 
     x = Convolution2D(squeeze, (1, 1), padding='valid', name=s_id + sq1x1)(x)
+    x = BatchNormalization()(x)
     x = Activation('relu', name=s_id + relu + sq1x1)(x)
 
     left = Convolution2D(expand, (1, 1), padding='valid', name=s_id + exp1x1)(x)
+    left = BatchNormalization()(left)
     left = Activation('relu', name=s_id + relu + exp1x1)(left)
 
     right = Convolution2D(expand, (3, 3), padding='same', name=s_id + exp3x3)(x)
+    right = BatchNormalization()(right)
     right = Activation('relu', name=s_id + relu + exp3x3)(right)
 
     x = concatenate([left, right], axis=channel_axis, name=s_id + 'concat')
@@ -40,21 +59,11 @@ def fire_module(x, fire_id, squeeze=16, expand=64):
 
 # Original SqueezeNet from paper.
 
-def SqueezeNet(include_top=True, weights='imagenet',
+def SqueezeNet(include_top=True,
                input_tensor=None, input_shape=None,
                classes=10):
     """Instantiates the SqueezeNet architecture.
     """
-
-    if weights not in {'imagenet', None}:
-        raise ValueError('The `weights` argument should be either '
-                         '`None` (random initialization) or `imagenet` '
-                         '(pre-training on ImageNet).')
-
-    if weights == 'imagenet' and classes != 1000:
-        raise ValueError('If using `weights` as imagenet with `include_top`'
-                         ' as true, `classes` should be 1000')
-
     input_shape = _obtain_input_shape(input_shape,
                                       default_size=32,
                                       min_size=32,
@@ -71,7 +80,7 @@ def SqueezeNet(include_top=True, weights='imagenet',
 
     x = Convolution2D(96, (3, 3), padding='same', name='conv1')(img_input)
     x = Activation('relu', name='relu_conv1')(x)
-    x = MaxPooling2D(pool_size=(2, 2), name='pool1')(x)
+    # x = MaxPooling2D(pool_size=(2, 2), name='pool1')(x)
 
     x = fire_module(x, fire_id=2, squeeze=16, expand=64)
     x = fire_module(x, fire_id=3, squeeze=16, expand=64)
@@ -86,15 +95,13 @@ def SqueezeNet(include_top=True, weights='imagenet',
 
     x = fire_module(x, fire_id=9, squeeze=64, expand=256)
 
+    x = BatchNormalization()(x)
     # x = Dropout(0.5, name='drop9')(x)
-    x = Convolution2D(1000, (1, 1), padding='valid', name='conv10')(x)
-    x = Activation('relu', name='relu_conv10')(x)
-    x = Convolution2D(classes, (1, 1), padding='valid', name='conv10b')(x)
-    x = Activation('relu', name='relu_conv10b')(x)
+    # x = Convolution2D(1000, (1, 1), padding='valid', name='conv10')(x)
+    x = Activation('relu', name='relu_10')(x)
     x = GlobalAveragePooling2D(name="avgpool10")(x)
-    # x = Dense(classes, name="softmax-10")(x)
-    # x = Activation('relu', name='relu_10')(x)
-    x = Activation('softmax', name='softmax')(x)
+    x = Dense(classes, activation='softmax', name="softmax-10")(x)
+    # x = Activation('softmax', name='softmax')(x)
 
     # Ensure that the model takes into account
     # any potential predecessors of `input_tensor`.
@@ -105,31 +112,166 @@ def SqueezeNet(include_top=True, weights='imagenet',
 
     model = Model(inputs, x, name='squeezenet')
 
-    # load weights
-    if weights == 'imagenet':
-        if include_top:
-            weights_path = get_file('squeezenet_weights_tf_dim_ordering_tf_kernels.h5',
-                                    WEIGHTS_PATH,
-                                    cache_subdir='models')
-        else:
-            weights_path = get_file('squeezenet_weights_tf_dim_ordering_tf_kernels_notop.h5',
-                                    WEIGHTS_PATH_NO_TOP,
-                                    cache_subdir='models')
-
-        model.load_weights(weights_path)
-
-        if K.image_data_format() == 'channels_first':
-            warnings.warn('You are using the TensorFlow backend, yet you '
-                          'are using the Theano '
-                          'image data format convention '
-                          '(`image_data_format="channels_first"`). '
-                          'For best performance, set '
-                          '`image_data_format="channels_last"` in '
-                          'your Keras config '
-                          'at ~/.keras/keras.json.')
     return model
 
 
+def euclidean_distance(inputs):
+    assert len(inputs) == 2, 'Euclidean distance needs 2 inputs, {} given'.format(len(inputs))
+    u, v = inputs
+    return K.sqrt(K.sum((K.square(u - v)), axis=1, keepdims=True))
+
+
+def contrastive_loss(y_true, y_pred):
+    margin = 1.
+    return K.mean((1. - y_true) * K.square(y_pred) + y_true * K.square(K.maximum(margin - y_pred, 0.)))
+
+
+def dd_make_img_pair(fpaths, identical=True):
+    folder1 = folder2 = np.random.choice(fpaths)
+    if not identical:
+        while os.path.samefile(folder1, folder2):
+            folder2 = np.random.choice(fpaths)
+    rgb_path1 = rgb_path2 = np.random.choice(glob(pjoin(folder1, "*.bmp")))
+    while os.path.samefile(rgb_path1, rgb_path2):
+        rgb_path2 = np.random.choice(glob(pjoin(folder2, "*.bmp")))
+    depth_path1 = rgb_path1[:-5] + "d.dat"
+    depth_path2 = rgb_path2[:-5] + "d.dat"
+
+    # process image 1
+    rgb1 = Image.open(rgb_path1)
+    rgb1.thumbnail((640, 480))
+    rgb1 = np.asarray(rgb1)[140:340, 220:420, :3]
+    depth1 = pd.read_csv(depth_path1, sep='\t', header=None)
+    depth1[(depth1 > 3000) | (depth1 == -1)] = 3000
+    depth1 = depth1.values[140:340, 220:420]
+    depth1 = (depth1 - np.mean(depth1)) / np.max(depth1)
+    rgbd1 = np.dstack((rgb1, depth1))
+
+    # process image 2
+    rgb2 = Image.open(rgb_path1)
+    rgb2.thumbnail((640, 480))
+    rgb2 = np.asarray(rgb2)[140:340, 220:420, :3]
+    depth2 = pd.read_csv(depth_path2, sep='\t', header=None)
+    depth2[(depth2 > 3000) | (depth2 == -1)] = 3000
+    depth2 = depth2.values[140:340, 220:420]
+    depth2 = (depth2 - np.mean(depth2)) / np.max(depth2)
+    rgbd2 = np.dstack((rgb2, depth2))
+
+    return np.array([rgbd1, rgbd2])
+
+
+def run():
+    batch_size = 32
+    num_classes = 10
+    epochs = 200
+
+    with tf.device("/cpu:0"):
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+        # Convert class vectors to binary class matrices.
+        y_train = to_categorical(y_train, num_classes)
+        y_test = to_categorical(y_test, num_classes)
+
+        x_train = x_train.astype('float32', copy=False)
+        x_test = x_test.astype('float32', copy=False)
+        x_train /= 255
+        x_test /= 255
+
+    optimizer = Adam(lr=0.001)
+
+    # input_shape = Input(shape=x_train.shape[1:])
+    squeezenet_model_file = './sqz_log/model.h5'
+    if os.path.exists(squeezenet_model_file):
+        model = load_model(squeezenet_model_file)
+    else:
+        # train a new SqueezeNet
+        model = SqueezeNet(classes=num_classes)
+
+        model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+        train_data = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True,
+                                        width_shift_range=0.125, height_shift_range=0.125, horizontal_flip=True)
+        validation_data = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
+
+        for data in (train_data, validation_data):
+            data.fit(x_train)
+
+        callbacks = [
+            LearningRateScheduler(partial(getitem, tuple(take(epochs, concat(
+                repeat(0.01, 1), repeat(0.1, 99), repeat(0.01, 50), repeat(0.001)))))),
+            ModelCheckpoint(filepath=squeezenet_model_file),
+            TensorBoard(log_dir="./sqz_log", batch_size=batch_size)
+        ]
+        results = model.fit_generator(train_data.flow(x_train, y_train, batch_size=batch_size),
+                                      steps_per_epoch=x_train.shape[0] // batch_size,
+                                      epochs=epochs,
+                                      callbacks=callbacks,
+                                      validation_data=validation_data.flow(x_test, y_test, batch_size=batch_size),
+                                      validation_steps=x_test.shape[0] // batch_size)
+
+        with open('./sqz_log/history.pickle', 'wb') as f:
+            pickle.dump(results.history, f)
+        save_model(model, squeezenet_model_file)
+
+    with tf.device("/cpu:0"):
+        model.summary()
+
+    # Build the siamese architecture
+    model.layers.pop()
+    input_shape = x_train.shape[1:]
+    im_in1 = Input(shape=input_shape)
+    im_in2 = Input(shape=input_shape)
+    feat_x1 = model(im_in1)
+    feat_x2 = model(im_in2)
+
+    lambda_merge = Lambda(euclidean_distance, output_shape=input_shape)([feat_x1, feat_x2])
+
+    siamese = Model(input=[im_in1, im_in2], output=lambda_merge, name="siamese")
+    siamese.summary()
+
+    siamese.compile(optimizer=optimizer, loss=contrastive_loss)
+
+    def make_img_pair(identical, from_train):
+        # TODO: Select the image pairs
+        if identical:
+            if from_train:
+                pass
+            else:
+                pass
+        else:
+            if from_train:
+                pass
+            else:
+                pass
+
+    def generator(from_train):
+        while True:
+            X = []
+            y = []
+            identical = True
+            for _ in range(batch_size):
+                X.append(make_img_pair(identical, from_train))
+                y.append(np.array([0.]))
+                identical = not identical
+            X = np.asarray(X)
+            y = np.asarray(y)
+            yield [X[:, 0], X[:, 1]], y
+
+    callbacks = [
+        LearningRateScheduler(partial(getitem, tuple(take(epochs, concat(
+            repeat(0.01, 1), repeat(0.1, 99), repeat(0.01, 50), repeat(0.001)))))),
+        ModelCheckpoint(filepath=squeezenet_model_file),
+        TensorBoard(log_dir="./siam_log", batch_size=batch_size)
+    ]
+    outputs = siamese.fit_generator(generator(from_train=True),
+                                    steps_per_epoch=x_train.shape[0] // batch_size,
+                                    epochs=epochs,
+                                    validation_data=generator(from_train=False),
+                                    validation_steps=x_test.shape[0] // batch_size, callbacks=callbacks)
+
+    with open('./siam_log/history.pickle', 'wb') as f:
+        pickle.dump(outputs.history, f)
+    save_model(siamese, './siam_log/siamese.h5')
+
+
 if __name__ == "__main__":
-    model = SqueezeNet(weights=None, classes=10)
-    model.summary()
+    run()
--- squeezenet.py
+++ squeezenet.py
@@ -10,18 +10,20 @@ from mlxtend.plotting import plot_confusion_matrix
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
-from data_set                  import load_data
-from funcy                     import concat, identity, juxt, partial, rcompose, repeat, take
-from keras.callbacks           import LearningRateScheduler
-from keras.layers              import Activation, Add, BatchNormalization, Concatenate, Conv2D, Dense, Dropout, GlobalAveragePooling2D, Input, MaxPooling2D
-from keras.models              import Model, save_model, load_model
-from keras.optimizers          import SGD
+from data_set import load_data
+from funcy import concat, identity, juxt, partial, rcompose, repeat, take
+from keras.callbacks import LearningRateScheduler
+from keras.layers import Activation, Add, BatchNormalization, Concatenate, Conv2D, Dense, Dropout, Flatten, \
+    GlobalAveragePooling2D, Input, Lambda, MaxPooling2D
+from keras.models import Model, save_model, load_model
+from keras.optimizers import SGD, Adam
 from keras.preprocessing.image import ImageDataGenerator
-from keras.regularizers        import l2
-from keras.utils               import plot_model
-from operator                  import getitem
+from keras.regularizers import l2
+from keras.utils import plot_model
+from operator import getitem
 from sklearn.metrics import classification_report, confusion_matrix
 import tensorflow as tf
+from keras import backend as K
 
 
 def computational_graph(class_size):
@@ -37,7 +39,8 @@ def computational_graph(class_size):
         return Activation('relu')
 
     def conv(filters, kernel_size):
-        return Conv2D(filters, kernel_size, padding='same', kernel_initializer='he_normal', kernel_regularizer=l2(0.0001))  # ReLUするならウェイトをHe初期化するのが基本らしい。あと、Kerasにはweight decayがなかったのでkernel_regularizerで代替したのたけど、これで正しい？
+        return Conv2D(filters, kernel_size, padding='same', kernel_initializer='he_normal', kernel_regularizer=l2(
+            0.0001))  # ReLUするならウェイトをHe初期化するのが基本らしい。あと、Kerasにはweight decayがなかったのでkernel_regularizerで代替したのたけど、これで正しい？
 
     def concatenate():
         return Concatenate()
@@ -92,65 +95,13 @@ def computational_graph(class_size):
                     dense(class_size, 'softmax'))
 
 
-def main():
-    import os
-    with tf.device("/cpu:0"):
-        (x_train, y_train), (x_validation, y_validation) = load_data()
-
-    batch_size = 32
-    epochs = 200
-
-    model_file = './results/model.h5'
-    if os.path.exists(model_file):
-        model = load_model(model_file)
-        with tf.device("/cpu:0"):
-            validation_data = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
-
-    else:
-        model = Model(*juxt(identity, computational_graph(y_train.shape[1]))(Input(shape=x_train.shape[1:])))
-        model.compile(loss='categorical_crossentropy', optimizer=SGD(momentum=0.9), metrics=['accuracy'])
-
-        with tf.device("/cpu:0"):
-            train_data = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True, width_shift_range=0.125, height_shift_range=0.125, horizontal_flip=True)
-            validation_data = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
-
-        for data in (train_data, validation_data):
-            data.fit(x_train)  # 実用を考えると、x_validationでのfeaturewiseのfitは無理だと思う……。
-
-        results = model.fit_generator(train_data.flow(x_train, y_train, batch_size=batch_size),
-                                      steps_per_epoch=x_train.shape[0] // batch_size,
-                                      epochs=epochs,
-                                      callbacks=[LearningRateScheduler(partial(getitem, tuple(take(epochs, concat(repeat(0.01, 1), repeat(0.1, 99), repeat(0.01, 50), repeat(0.001))))))],
-                                      validation_data=validation_data.flow(x_validation, y_validation, batch_size=batch_size),
-                                      validation_steps=x_validation.shape[0] // batch_size)
-
-        with open('./results/history.pickle', 'wb') as f:
-            pickle.dump(results.history, f)
-        save_model(model, model_file)
-
-    try:
-        with tf.device("/cpu:0"):
-            model.summary()
-            # plot_model(model, to_file='./results/model.png')
-    except Exception as ex:
-        print("plot_model failed with error:", repr(ex), "\nMoving on...")
-
+def generate_confusion_matrix(model, x_validation, y_validation, batch_size):
     # Confution Matrix and Classification Report
-    # Y_pred = model.predict_generator(validation_data.flow(x_validation, None, batch_size=batch_size), 1 + (x_validation.shape[0]// batch_size))
     Y_pred = model.predict(x_validation, batch_size=batch_size)
-    # print("Y_pred:", Y_pred.shape)
-    # print("x_validation:", x_validation.shape)
-    # print("y_validation:", y_validation.shape)
     y_pred = np.argmax(Y_pred, axis=1)
     y_valid = np.argmax(y_validation, axis=1)
-    # print("y_pred:", y_pred.shape)
-    # print("y_valid:", y_valid.shape)
-    # print(np.argmax(y_validation, axis=1))
-    # print(len(y_pred))
 
     print('Confusion Matrix')
-    # plt.imshow((y_valid, y_pred))
-    # plt.show()
     cm = (confusion_matrix(y_valid, y_pred))
     print(cm)
     fig, ax = plot_confusion_matrix(conf_mat=cm,
@@ -159,6 +110,108 @@ def main():
                                     show_normed=True)
     plt.show()
     del model
+
+
+def euclidean_distance(inputs):
+    assert len(inputs) == 2, 'Euclidean distance needs 2 inputs, {} given'.format(len(inputs))
+    u, v = inputs
+    return K.sqrt(K.sum((K.square(u - v)), axis=1, keepdims=True))
+
+
+def contrastive_loss(y_true, y_pred):
+    margin = 1.
+    return K.mean((1. - y_true) * K.square(y_pred) + y_true * K.square(K.maximum(margin - y_pred, 0.)))
+
+
+def siamese(im_in, squeezenet_model):  # modelsqueeze): output of squeezenet model
+    # squeezenet_model = Dense(512, activation="relu")(squeezenet_model)
+    # squeezenet_model = Dropout(0.2)(squeezenet_model)
+    last_layer = squeezenet_model.layers[-1]
+    # print("last_layer: name:", last_layer.name, "shape:", last_layer.output.shape)
+    # d1 = Dense(128, activation="linear")
+    # d1.build((None, 512))  # , int(last_layer.output.shape[1])))=
+    # squeezenet_model.layers.append(d1)
+    # l1 = Lambda(lambda x: K.l2_normalize(x, axis=1))
+    # l1.build((None, 512))  # , int(last_layer.output.shape[1])))
+    # squeezenet_model.layers.append(l1)
+    squeezenet_model.summary()
+    # return None
+
+    model_top = squeezenet_model
+    # feat_x = Dense(128, activation="linear")(last_layer.output)
+    # feat_x = Lambda(lambda x: K.l2_normalize(x, axis=1))(feat_x)
+    model_top = Model(inputs=[im_in], outputs=last_layer.output)
+    model_top.summary()
+
+    print("im_in shape:", im_in.shape, "squeezenet_model type:", type(squeezenet_model))
+    im_in1 = Input(shape=im_in.shape)
+    im_in2 = Input(shape=im_in.shape)
+
+    feat_x1 = model_top(im_in1)
+    feat_x2 = model_top(im_in2)
+
+    lambda_merge = Lambda(euclidean_distance)([feat_x1, feat_x2])
+
+    model_final = Model(inputs=[im_in1, im_in2], outputs=lambda_merge)
+    model_final.summary()
+
+    adam = Adam(lr=0.001)
+
+    # sgd = SGD(lr=0.001, momentum=0.9)
+
+    model_final.compile(optimizer=adam, loss=contrastive_loss)
+
+
+def main():
+    import os
+    with tf.device("/cpu:0"):
+        (x_train, y_train), (x_validation, y_validation) = load_data()
+
+    batch_size = 32
+    epochs = 200
+    input_shape = Input(shape=x_train.shape[1:])
+    model_file = './results/model.h5'
+    if os.path.exists(model_file):
+        model = load_model(model_file)
+        # with tf.device("/cpu:0"):
+        #     validation_data = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
+    else:
+        model = Model(*juxt(identity, computational_graph(y_train.shape[1]))(input_shape))
+        model.compile(loss='categorical_crossentropy', optimizer=SGD(momentum=0.9), metrics=['accuracy'])
+
+        with tf.device("/cpu:0"):
+            train_data = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True,
+                                            width_shift_range=0.125, height_shift_range=0.125, horizontal_flip=True)
+            validation_data = ImageDataGenerator(featurewise_center=True, featurewise_std_normalization=True)
+
+        for data in (train_data, validation_data):
+            data.fit(x_train)  # 実用を考えると、x_validationでのfeaturewiseのfitは無理だと思う……。
+
+        results = model.fit_generator(train_data.flow(x_train, y_train, batch_size=batch_size),
+                                      steps_per_epoch=x_train.shape[0] // batch_size,
+                                      epochs=epochs,
+                                      callbacks=[LearningRateScheduler(partial(getitem, tuple(take(epochs, concat(
+                                          repeat(0.01, 1), repeat(0.1, 99), repeat(0.01, 50), repeat(0.001))))))],
+                                      validation_data=validation_data.flow(x_validation, y_validation,
+                                                                           batch_size=batch_size),
+                                      validation_steps=x_validation.shape[0] // batch_size)
+
+        with open('./results/history.pickle', 'wb') as f:
+            pickle.dump(results.history, f)
+        save_model(model, model_file)
+
+    try:
+        with tf.device("/cpu:0"):
+            # model.summary()
+            # print("=== AFTER POPPING THE LAST ===")
+            model.layers.pop()
+            # model.summary()
+            # generate_confusion_matrix(model, x_validation, y_validation, batch_size)
+            # plot_model(model, to_file='./results/model.png')
+    except Exception as ex:
+        print("plot_model failed with error:", repr(ex), "\nMoving on...")
+
+    siamese(input_shape, model)
 
 
 if __name__ == '__main__':
